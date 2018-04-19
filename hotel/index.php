bb5dc9cd77699e3ff1af7be14e03d0b664b7563f
Samuel Aniefiok <coolgen4@outlook.com>
2018-04-19 05:30:15 +0100

    Add files via upload

---
+++ hotel/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>HNG INTERNSHIP  4.0  </title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div id="menu">
+<ul>
+  <li><a class="active" href="#logo">COOLSAMI@HNG INERNSHIP 4.0</a></li>
+   
+  <li style="float:right"><a href="#contact">CONTACT</a></li>
+  <li style="float:right"><a href="#about">ABOUTUS</a></li>
+  <li style="float:right"><a href="#home">HOME</a></li>
+</ul>
+</div>
+
+<div id="bluet">
+
+	<h2>               WELCOME TO HNG INTERNSHIP </h2>
+                            
+                                <h2>A PLACE WHERE</h2>
+
+               <h2>ASPIRING SOFTWARE DEVELOPERS</h2>
+
+                           <h2>ACHIEVE THEIR OBJECTIVES</h2>
+                           <?php
+                           echo $date = date('Y-m-d G:m:t');  
+                           ?>
+
+	
+</div>
+
+	
+
+		
+	
+</body>
+
+
+</html>
